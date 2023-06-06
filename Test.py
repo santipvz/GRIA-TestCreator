efd280abc8f41b826af546e816b0d352a2ea1dc8
SantiagoRR2004 <114857078+SantiagoRR2004@users.noreply.github.com>
2023-06-06 20:33:20 +0200

    Actualizar el creador

--- Test.py
+++ Test.py
@@ -16,7 +16,6 @@ def questionGenerator(numOfQuestions = 30):
             numOfQuestionsInUnit = len(questionsData["questions"])
 
             enoughQuestions = (questionsOfEachUnit[unit] <= numOfQuestionsInUnit)
-            print(enoughQuestions)
 
             while questionsOfEachUnit[unit] > 0:
                 x = randint(0, numOfQuestionsInUnit-1)
@@ -34,53 +33,133 @@ def questionGenerator(numOfQuestions = 30):
 
     return questions
 
+def examWriter(questions, fileOutName):
+    html_head = """
+    <html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Empresas</title>
+        <style>
+            .question {
+                margin-bottom: 20px;
+            }
+            .question p {
+                font-weight: bold;
+                font-size: 20px;
+            }
+            .question li {
+                font-size: 18px;
+                list-style-type: none;
+            }
+            .correct-answer {
+                display: none;
+                font-weight: bold;
+                font-size: 16px;
+            }
+            .answered {
+                pointer-events: none;
+            }
+        </style>
+        <script>
+            function submitForm() {
+                var form = document.getElementById("testForm");
+                var answers = [];
+                var questions = document.getElementsByClassName("question");
+                for (var i = 0; i < questions.length; i++) {
+                    var question = questions[i];
+                    var inputs = question.getElementsByTagName("input");
+                    var answer = null;
+                    for (var j = 0; j < inputs.length; j++) {
+                        if (inputs[j].checked) {
+                            answer = inputs[j].value;
+                            break;
+                        }
+                    }
+                    answers.push(answer);
+                }
+                
+                // Mostrar las respuestas después de enviar el formulario
+                var correctAnswers = document.getElementsByClassName("correct-answer");
+                for (var i = 0; i < correctAnswers.length; i++) {
+                    correctAnswers[i].style.display = "block";
+                }
+                
+                // Bloquear las opciones después de enviar el formulario
+                var options = document.getElementsByTagName("input");
+                for (var i = 0; i < options.length; i++) {
+                    options[i].classList.add("answered");
+                }
+                
+                // Calcular puntuación total
+                var score = calculateScore(answers);
+                var scoreDisplay = document.getElementById("scoreDisplay");
+                scoreDisplay.innerHTML = "Puntuación total: " + score;
+                
+                return false;
+            }
+            
+            function calculateScore(answers) {
+                var correctCount = 0;
+                for (var i = 0; i < answers.length; i++) {
+                    var questionIndex = i;
+                    var correctOptionInput = document.getElementsByName("correct_" + questionIndex)[0];
+                    var correctOption = correctOptionInput.value;
+                    if (answers[i] === correctOption) {
+                        correctCount++;
+                    }
+                }
+                var totalOptions = answers.length;
+                var score = (correctCount / totalOptions) * 100;
+                return score.toFixed(2) + "%";
+            }
+        </script>
+    </head>
+    <body>
+        <form id="testForm" onsubmit="return submitForm()">
+    """
+
+    html_tail='''
+            <input type="submit" value="Enviar respuestas">
+        </form>
+        <p id="scoreDisplay"></p>
+    </body>
+    </html>
+    '''
+    fileOut = open(fileOutName, "w", encoding="utf-8")
+    fileOut.write(html_head)
+
+    for i, question in enumerate(questions[:numOfQuestions]):
+        question_number = i + 1  ## Incrementar el número de pregunta
+        
+        question_text = question["question"]
+        options = question["options"]
+        correct_option = question["correct_option"]
+        option_html = ""
+        for j, option in enumerate(options):
+            option_letter = chr(ord("A") + j)  ## Convertir el índice en una letra
+            option_html += f'<li><input type="radio" name="question_{i}" value="{option_letter}"> {option_letter}) {option}</li>'
+        
+        correct_html = f'<p class="correct-answer">Respuesta correcta: {chr(ord("A") + correct_option)}</p>'
+        correct_hidden_input = f'<input type="hidden" name="correct_{i}" value="{chr(ord("A") + correct_option)}">'
+        
+        fileOut.write(f"""
+        <div class="question">
+            <p>{question_number}: {question_text}</p>  <!-- Mostrar el número de pregunta -->
+            <ul>
+                {option_html}
+            </ul>
+            {correct_html}
+            {correct_hidden_input}
+        </div>
+        """)
+
+    fileOut.write(html_tail)
+    fileOut.close()
 
 
-fileOutName = "./ExamenTest.html"
-
-html_head = """
-<html>
-<head>
-    <title>Empresas</title>
-</head>
-</body>
-"""
-
-html_tail='''
-</body>
-</hmtl>
-'''
-
+output = "./ExamenTest.html"
 numOfQuestions = 30
 
 questions = questionGenerator(numOfQuestions)
 
-fileOut = open(fileOutName, "w")
-fileOut.write(html_head)
-
-for question in questions:
-    question_text = question["question"]
-    options = question["options"]
-    option_html = ""
-    for i, option in enumerate(options):
-        option_letter = chr(ord("A") + i)  # Convert index to letter
-        option_html += f"<li>{option_letter}) {option}</li>"
-    correct_html = "Correct:" + chr(ord("A") + question["correct_option"])
-    fileOut.write(f"""
-    <div>
-        <p>{question_text}</p>
-        <ul>
-            {option_html}
-        </ul>
-        <p>{correct_html}</p>
-    </div>
-    """)
-
-fileOut.write(html_tail)
-fileOut.close()
-
-
-
-
-
-
+examWriter(questions, output)
