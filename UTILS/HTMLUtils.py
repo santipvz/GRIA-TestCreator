cb30d08609e794a5383932268bda4419e7e4d99f
Deibis <135325970+deibis4211@users.noreply.github.com>
2025-03-15 13:26:26 +0100

    Update HTMLUtils.py

--- UTILS/HTMLUtils.py
+++ UTILS/HTMLUtils.py
@@ -5,59 +5,59 @@ from UTILS import questionTypes
 
 def examWriter(questions, fileOutName):
     submitFormFunction = """function submitForm() {
-        var form = document.getElementById("testForm");
-        form.elements["submitButton"].disabled = true;
+    var form = document.getElementById("testForm");
+    form.elements["submitButton"].disabled = true;
 
-        var correctGlobal = 0;
-        var incorrectGlobal = 0;
-        var unanswered = 0;
-        var totalScore = 0;
+    var correctGlobal = 0;
+    var incorrectGlobal = 0;
+    var unanswered = 0;
+    var totalScore = 0;
 
-        var questions = document.getElementsByClassName("singleChoice");
+    var questions = document.getElementsByClassName("singleChoice");
 
-        for (var question of questions) {
-            var score = singleChoice(question);
-            if (score === 1) {
-                correctGlobal++;
-                totalScore += score;
-            } else if (isNaN(score)) {
-                unanswered++;
-            } else {
-                incorrectGlobal++;
-                totalScore += score;
-            }
+    for (var question of questions) {
+        var score = singleChoice(question);
+        if (score === 1) {
+            correctGlobal++;
+            totalScore += score;
+        } else if (isNaN(score)) {
+            unanswered++;
+        } else {
+            incorrectGlobal++;
+            totalScore += score;
         }
-
-        var questions = document.getElementsByClassName("multipleChoice");
-
-        for (var question of questions) {
-            var score = multipleChoice(question);
-            if (score === 1) {
-                correctGlobal++;
-                totalScore += score;
-            } else if (isNaN(score)) {
-                unanswered++;
-            } else {
-                incorrectGlobal++;
-                totalScore += score;
-            }
-        }
-
-        console.log("Score out of all questions: " + totalScore);
-        console.log("Correct: " + correctGlobal);
-        console.log("Incorrect: " + incorrectGlobal);
-        console.log("Unanswered: " + unanswered);
-
-        // We calculate the score out of 10
-        totalScore = ((totalScore / (correctGlobal + incorrectGlobal + unanswered)) * 10).toFixed(2);
-
-        console.log("Score out of 10: " + totalScore);
-
-        var scoreDisplay = document.getElementById("scoreDisplay");
-        scoreDisplay.innerHTML = "Preguntas acertadas: " + correctGlobal + "<br>Preguntas falladas: " + incorrectGlobal + "<br>Puntuación total: " + totalScore + "/10";
-
-        return false;
     }
+
+    var questions = document.getElementsByClassName("multipleChoice");
+
+    for (var question of questions) {
+        var score = multipleChoice(question);
+        if (score === 1) {
+            correctGlobal++;
+            totalScore += score;
+        } else if (isNaN(score)) {
+            unanswered++;
+        } else {
+            incorrectGlobal++;
+            totalScore += score;
+        }
+    }
+
+    console.log("Score out of all questions: " + totalScore);
+    console.log("Correct: " + correctGlobal);
+    console.log("Incorrect: " + incorrectGlobal);
+    console.log("Unanswered: " + unanswered);
+
+    // We calculate the score out of 10
+    totalScore = ((totalScore / (correctGlobal + incorrectGlobal + unanswered)) * 10).toFixed(2);
+
+    console.log("Score out of 10: " + totalScore);
+
+    var scoreDisplay = document.getElementById("scoreDisplay");
+    scoreDisplay.innerHTML = "Preguntas acertadas: " + correctGlobal + "<br>Preguntas falladas: " + incorrectGlobal + "<br>Puntuación total: " + totalScore + "/10";
+
+    return false;
+}
 """
 
     singleChoiceFunction = """function singleChoice(question) {
@@ -97,65 +97,67 @@ def examWriter(questions, fileOutName):
 """
 
     multipleChoiceFunction = """function multipleChoice(question) {
-        var selectedInputs = question.querySelectorAll("input:checked");
+    var selectedInputs = question.querySelectorAll("input:checked");
 
-        // Get the correct answer element
-        var correctAnswer = question.querySelector(".correct-answer");
+    // Get the correct answer element
+    var correctAnswer = question.querySelector(".correct-answer");
 
-        // Extract the correct answers from the hidden input field
-        var correctAnswers = question.querySelector("input[name^='correct_']");
-        var correctAnswersArray = correctAnswers ? correctAnswers.value.split(",").map(x => x.trim()) : [];
+    // Extract the correct answers from the hidden input field
+    var correctAnswers = question.querySelector("input[name^='correct_']");
+    var correctAnswersArray = correctAnswers ? correctAnswers.value.split(",").map(x => x.trim()) : [];
 
-        // Display the correct answers
-        question.querySelectorAll(".correct-answer").forEach(x => x.style.display = "block");
+    // Display the correct answers
+    question.querySelectorAll(".correct-answer").forEach(x => x.style.display = "block");
 
-        // Get the total number of checkbox options
-        var options = question.querySelectorAll("input[type=checkbox]");
-        var numOptions = options.length;
+    // Get the total number of checkbox options
+    var options = question.querySelectorAll("input[type=checkbox]");
+    var numOptions = options.length;
 
-        // Disable all checkboxes to prevent changing the answer
-        options.forEach(input => input.disabled = true);
+    // Disable all checkboxes to prevent changing the answer
+    options.forEach(input => input.disabled = true);
 
-        var correct = 0;
-        var incorrect = 0;
-        var unanswered = 0;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
-        // Iterate over selected inputs to mark correct/incorrect answers
-        for (var selectedInput of selectedInputs) {
-            var listItem = selectedInput.closest("li"); // Get the <li> containing the checkbox
+    // Iterate over selected inputs to mark correct/incorrect answers
+    for (var selectedInput of selectedInputs) {
+        var listItem = selectedInput.closest("li"); // Get the <li> containing the checkbox
 
-            if (listItem) {
-                if (correctAnswersArray.includes(selectedInput.value)) {
-                    // listItem.classList.add("correct"); // Apply class to the entire list item
-                    correct++;
-                } else {
-                    // listItem.classList.add("incorrect");
-                    incorrect++;
-                }
+        if (listItem) {
+            if (correctAnswersArray.includes(selectedInput.value)) {
+                correct++;
+            } else {
+                incorrect++;
             }
         }
-
-        // We log the number of the question and score
-
-        // If all the correct answers are selected and no incorrect answers are selected
-        // we return 1
-        if (correct === correctAnswersArray.length && incorrect === 0) {
-            correctAnswer.classList.add("correct");
-            console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: 1");
-            return 1;
-        } // If there are no answers we return NaN
-        else if (selectedInputs.length === 0) {
-            correctAnswer.classList.add("incorrect");
-            console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: NaN");
-            return NaN;
-        } // If there are incorrect answers we return the penalty for random guessing
-        else {
-            correctAnswer.classList.add("incorrect");
-            console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: -1/" + (numOptions - 1));
-            return -1 / (numOptions - 1);
-        }
-
     }
+
+    // We log the number of the question and score
+
+    // If at least one correct answer is selected and no incorrect answers are selected
+    // we return 1
+    if (correct > 0 && incorrect === 0) {
+        correctAnswer.classList.add("correct");
+        console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: 1");
+        return 1;
+    } // If there are no answers we return NaN
+    else if (selectedInputs.length === 0) {
+        correctAnswer.classList.add("incorrect");
+        console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: NaN");
+        return NaN;
+    } // If no correct answers are selected, we return -0.5
+    else if (correct === 0) {
+        correctAnswer.classList.add("incorrect");
+        console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: -0.5");
+        return -0.5;
+    } // If there are incorrect answers we return the penalty for random guessing
+    else {
+        correctAnswer.classList.add("incorrect");
+        console.log(question.querySelector("p").innerHTML.split(":")[0] + " - Score: 0");
+        return 0;
+    }
+}
 """
 
     html_head = """
