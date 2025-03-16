7115cc6257ff6b75d78f10508ec744803f5e2a78
usbt0p <lucascdeaa@gmail.com>
2025-03-16 17:46:50 +0100

    format with black and unchange main.py

--- UTILS/formsScraper.py
+++ UTILS/formsScraper.py
@@ -4,9 +4,9 @@ import json
 
 
 def get_form_data(url: str = None, use_file: str = "") -> tuple[list, list, list]:
-    '''Extract questions, answers and question types from a google form URL.
-    The "structured" data in forms is buried in a script at the bottom of the HTML, so 
-    we need to work around it. 
+    """Extract questions, answers and question types from a google form URL.
+    The "structured" data in forms is buried in a script at the bottom of the HTML, so
+    we need to work around it.
 
     Parameters
     ----------
@@ -23,10 +23,10 @@ def get_form_data(url: str = None, use_file: str = "") -> tuple[list, list, list
             The list of answers.
     question_type : list
             The list of question types.
-    '''
+    """
 
     if use_file:
-        with open('test.html', 'r') as file:
+        with open("test.html", "r") as file:
             url_content = file.read()
     else:
         url_client = urlopen(url)
@@ -38,11 +38,10 @@ def get_form_data(url: str = None, use_file: str = "") -> tuple[list, list, list
     content = parser.find_all("script")
 
     # filter to get the one with 'var FB_PUBLIC_LOAD_DATA_' in it.
-    content = [
-        item for item in content if 'var FB_PUBLIC_LOAD_DATA_' in str(item)][0]
+    content = [item for item in content if "var FB_PUBLIC_LOAD_DATA_" in str(item)][0]
 
     # now, split the content to get the data. Extract the json in the var and remove the semicolon.
-    content = content.text.split('var FB_PUBLIC_LOAD_DATA_ = ')[1][:-1]
+    content = content.text.split("var FB_PUBLIC_LOAD_DATA_ = ")[1][:-1]
 
     # use the json format to turn the string into list.
     content = json.loads(content)
@@ -67,8 +66,8 @@ def get_form_data(url: str = None, use_file: str = "") -> tuple[list, list, list
     return questions, answers, question_type
 
 
-def format_into_json(questions, answers, type, filepath=None) -> dict :
-    '''Format the data into a json file for the quiz app.
+def format_into_json(questions, answers, type, filepath=None) -> dict:
+    """Format the data into a json file for the quiz app.
     For format of the json file, refer to the README.
 
     Parameters
@@ -86,9 +85,9 @@ def format_into_json(questions, answers, type, filepath=None) -> dict :
     -------
     dict_qs : dict
             The dictionary of questions.
-    '''
+    """
 
-    dict_qs = {'questions': []}
+    dict_qs = {"questions": []}
 
     for q, a, t in zip(questions, answers, type):
         if t == 2:
@@ -97,20 +96,20 @@ def format_into_json(questions, answers, type, filepath=None) -> dict :
                 "question": q,
                 "options": a,
                 "correct_option": None,
-                "questionType": type
+                "questionType": type,
             }
         elif t == 4:
             type = "multipleChoice"
             dict_q = {
                 "question": q,
                 "options": a,
-                "correct_options": [], # TODO implement a way to get this
-                "questionType": type
+                "correct_options": [],  # TODO implement a way to get this
+                "questionType": type,
             }
-        dict_qs['questions'].append(dict_q)
+        dict_qs["questions"].append(dict_q)
 
     if filepath:
-        with open(filepath, 'w') as file:
+        with open(filepath, "w") as file:
             json.dump(dict_qs, file, indent=4)
 
     return dict_qs
@@ -122,29 +121,29 @@ def get_correct_answers(html_content, class_id):
     # also, you have to manually search for the class of the div containing the correct answers (class_id), which is shit
     parser = soup(html_content, "html.parser")
     correct_answers_divs = parser.find_all("div", class_=class_id)
-    
+
     correct_answers = []
     for div in correct_answers_divs:
         answer_spans = div.find_all("span", dir="auto")
         for span in answer_spans:
             correct_answers.append(span.text)
-    
+
     return correct_answers
 
 
-if __name__ == '__main__':
-    
-	# Example usage
-    url = input('Enter the url: ')
-    name = input('Enter complete file name: ')
-    assert name[-5:] == '.json'
-    
+if __name__ == "__main__":
+
+    # Example usage
+    url = input("Enter the url: ")
+    name = input("Enter complete file name: ")
+    assert name[-5:] == ".json"
+
     # q, a, t = get_data(url=url, use_file='test_answers.html')
-    q, a, t = get_form_data(url=url) # questions, answers, question type
+    q, a, t = get_form_data(url=url)  # questions, answers, question type
     json_output = format_into_json(q, a, t, filepath=name)
 
-    '''# Example usage of get_correct_answers
+    """# Example usage of get_correct_answers
     with open('test_answers.html', 'r') as file:
         html_content = file.read()
     correct_answers = get_correct_answers(html_content, 'D42QGf')
-    print(correct_answers)'''
+    print(correct_answers)"""
--- main.py
+++ main.py
@@ -1,14 +1,9 @@
 from UTILS import exam
-import os
+
 
 numExams = 1
-numOfQuestions = 2
+numOfQuestions = 10
 folder = "PIC"
 
-# get current dir for testing a specific file that was added
-currentDir = os.getcwd()
-folder = os.path.join(currentDir, folder)
-questionsPerTopic = {os.path.join(folder, "Unit4Teacher.json"): 10}
-
 if __name__ == "__main__":
-    exam.examGenerator(folder, numExams, numOfQuestions, questionsPerTopic)
+    exam.examGenerator(folder, numExams, numOfQuestions)
